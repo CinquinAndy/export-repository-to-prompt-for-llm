929ec12c289ebd05883412b8204490baeb6f41f5
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-20 11:35:06 +0100

    ✨ Refactor code for dynamic argument handling
    Adjust script to handle optional arguments more flexibly. Use conditional checks to append arguments as needed when spawning the Python process.

--- index.js
+++ index.js
@@ -1,14 +1,24 @@
 #!/usr/bin/env node
 
-const {spawn} = require('child_process');
+const { spawn } = require('child_process');
 const path = require('path');
 
-const pythonScript = path.join(__dirname, 'export_repository_to_file.py');
+const pythonScript = path.join(__dirname, 'export-repository-to-file.py');
 const projectPath = process.argv[2];
-const preamblePath = process.argv[3];
-const outputPath = process.argv[4];
+const preamblePath = process.argv[3] || '';
+const outputPath = process.argv[4] || '';
 
-const pythonProcess = spawn('python', [pythonScript, projectPath, '-p', preamblePath, '-o', outputPath]);
+const args = [pythonScript, projectPath];
+
+if (preamblePath) {
+    args.push('-p', preamblePath);
+}
+
+if (outputPath) {
+    args.push('-o', outputPath);
+}
+
+const pythonProcess = spawn('python', args);
 
 pythonProcess.stdout.on('data', (data) => {
     console.log(data.toString());

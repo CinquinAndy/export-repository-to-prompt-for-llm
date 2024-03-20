77968c482485eeb07e69c90120e51ff1687be7a9
Cinquin Andy <andy.cinquin@infotel.com>
2024-03-20 12:14:47 +0100

    ✨ Update version to 1.0.2, install Python dependencies in script
    - Updated package.json version to 1.0.2
    - Added installation of Python dependencies in the script using pip
    - Improved error handling for Python dependency installation and script execution

--- index.js
+++ index.js
@@ -1,33 +1,49 @@
 #!/usr/bin/env node
 
-const { spawn } = require('child_process');
+const {spawn} = require('child_process');
 const path = require('path');
 
-const pythonScript = path.join(__dirname, 'export-repository-to-file.py');
-const projectPath = process.argv[2];
-const preamblePath = process.argv[3] || '';
-const outputPath = process.argv[4] || '';
-
-const args = [pythonScript, projectPath];
-
-if (preamblePath) {
-    args.push('-p', preamblePath);
-}
-
-if (outputPath) {
-    args.push('-o', outputPath);
-}
-
-const pythonProcess = spawn('python', args);
-
-pythonProcess.stdout.on('data', (data) => {
+// Install Python dependencies
+const installDeps = spawn('pip', ['install', '-r', path.join(__dirname, 'requirements.txt')]);
+installDeps.stdout.on('data', (data) => {
     console.log(data.toString());
 });
-
-pythonProcess.stderr.on('data', (data) => {
+installDeps.stderr.on('data', (data) => {
     console.error(data.toString());
 });
+installDeps.on('close', (code) => {
+    if (code !== 0) {
+        console.error(`Failed to install Python dependencies. Exited with code ${code}`);
+        process.exit(1);
+    }
 
-pythonProcess.on('close', (code) => {
-    console.log(`Python script exited with code ${code}`);
+
+    const pythonScript = path.join(__dirname, 'export-repository-to-file.py');
+    const projectPath = process.argv[2];
+    const preamblePath = process.argv[3] || '';
+    const outputPath = process.argv[4] || '';
+
+    const args = [pythonScript, projectPath];
+
+    if (preamblePath) {
+        args.push('-p', preamblePath);
+    }
+
+    if (outputPath) {
+        args.push('-o', outputPath);
+    }
+
+    const pythonProcess = spawn('python', args);
+
+    pythonProcess.stdout.on('data', (data) => {
+        console.log(data.toString());
+    });
+
+    pythonProcess.stderr.on('data', (data) => {
+        console.error(data.toString());
+    });
+
+    pythonProcess.on('close', (code) => {
+        console.log(`Python script exited with code ${code}`);
+    });
 });
